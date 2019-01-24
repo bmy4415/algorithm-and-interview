ecd187bd1189504e3e88a3df6281be4389c9a3ca
bmy4415 <bmy4415@naver.com>
2019-01-24 19:18:43 +0900

    https://leetcode.com/problems/counting-bits/ 문제 pretify

--- leet_code/algorithm-338.py
+++ leet_code/algorithm-338.py
@@ -22,17 +22,10 @@ class Solution:
 
         # initialize arr
         arr = [0] * (num+1)
-        arr[0] = 0
+        for i in range(1, num+1):
+            arr[i] = arr[int(i/2)] + i%2
 
-        i = 1
-        while True:
-            if i > num:
-                return arr[:num+1]
-
-            for j in range(i, min(2*i, num)+1):
-                arr[j] = 1 + arr[j-i]
-
-            i *= 2
+        return arr
 
 s = Solution()
 print(s.countBits(10))
